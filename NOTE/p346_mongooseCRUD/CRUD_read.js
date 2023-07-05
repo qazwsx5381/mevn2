7828a2817eab42520a2ef43338c0c1c3f8119faa
qazwsx5381 <qazwsx5381@gmail.com>
2023-07-05 17:48:12 +0900

    230705 최종

--- NOTE/p346_mongooseCRUD/CRUD_read.js
+++ NOTE/p346_mongooseCRUD/CRUD_read.js
@@ -1,12 +1,14 @@
 const mycol = require('./CRUD_ori')
 
 const main = async () => {
-  const t = await mycol.find(
-    {
-      title: { $eq: '홍길동' }
-    },
-    { _id: 0 }
-  ).lean()
+  const t = await mycol
+    .find(
+      {
+        title: { $eq: '홍길동' }
+      },
+      { _id: 0 }
+    )
+    .lean() // p349 설명 효율적 메소드
   console.log(t)
 }
 
